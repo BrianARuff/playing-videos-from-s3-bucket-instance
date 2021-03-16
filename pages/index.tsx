95f98413798d5e3760daf45c2e2cdf5f9195a210
brianaruff <brff19@gmail.com>
2021-03-16 10:07:18 -0400

    added button to app with input to play at specified seconds

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,31 @@
 import * as React from 'react'
-import {useEffect, useState} from "react";
+import {LegacyRef, Ref, RefObject, useEffect, useState} from "react";
 import ReactPlayer from "react-player";
-import {Typography} from "@material-ui/core";
+import {Typography, Button, Input, InputLabel} from "@material-ui/core";
 
 
 export default function Home() {
-    const [videoURL, setvideoURL] = useState(null);
+    const [inputData, setInputData] = useState({value: ""});
+    const videoPlayer = React.createRef<ReactPlayer>();
+    const [videoURL, setVideoURL] = useState();
     useEffect(() => {
-        fetch("/api/hello") .then(r => r.json()).then(s => {
-            setvideoURL(s.data);
+        fetch("/api/hello").then(r => r.json()).then(({data}) => {
+            setVideoURL(data);
         })
-    }, [])
+    }, []);
+    const handlePlayVideo = e => {
+        console.log(videoPlayer.current.seekTo(Number(inputData.value)));
+    }
+    const handleInputChange = e => setInputData(() => ({[e.target.name]: e.target.value}))
     return (
         <>
-           <Typography variant="h1">ASMR</Typography>
+            <Typography variant="h1">ASMR</Typography>
             <Typography variant="h4">Car Wash</Typography>
-            <ReactPlayer url={videoURL} controls autoPlay/>
-
+            <ReactPlayer ref={videoPlayer} url={videoURL} controls autoPlay/>
+            <div style={{margin: "1rem  0"}}>
+                <Input placeholder="Time stamp in seconds" onChange={handleInputChange} name="value"/>
+            </div>
+            <Button onClick={handlePlayVideo} variant="contained" color="primary" fullWidth={true}>Play at specified value</Button>
         </>
     )
 }
